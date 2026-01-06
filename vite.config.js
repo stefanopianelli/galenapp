55261e954d34f81b550b2e07c183c5878cbdc0e5
Stefano Pianelli <stefanopianelli@gmail.com>
2026-01-06 14:10:26 +0100

    build: configurato manual chunking per ottimizzare la build
    - Aggiunta la configurazione 'manualChunks' in vite.config.js.
    - Separate le librerie pesanti (react, jspdf) in chunk individuali.
    - Risolto l'avviso sulle dimensioni dei chunk e migliorato il performance di caricamento iniziale.

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,18 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),
   			tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('react-dom') || id.includes('react')) {
+            return 'react';
+          }
+          if (id.includes('jspdf') || id.includes('jspdf-autotable')) {
+            return 'jspdf';
+          }
+        }
+      }
+    }
+  }
 })
